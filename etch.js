fe28ab31a78a1bc571c06e8223c5703c840acc32
maialele <mayalapi@gmail.com>
2022-09-21 15:52:30 +0300

    coloring works

--- etch.js
+++ etch.js
@@ -7,12 +7,32 @@ function makeRows(rows, cols) {
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
+    cell.addEventListener("click", addColor);
     container.appendChild(cell).className = "grid-item";
   };
-};
+  
+}
+
+function addColor(event) {
+  event.target.style.background = "red"
+}
+
 
 function reset(rows, cols) {
   container.innerHTML = ""
 }
 
+
 makeRows(12, 12);
+
+
+
+
+
+
+      // var item = document.getElementsByClassName("grid-item");
+      // for (let i = 0; i < item.length; i++) {
+      //   item[i].addEventListener('click', function () {
+      //     document.getElementsByClassName("grid-item").style.backgroundcolor = "red";
+      //   });
+    // }
