ce21d6d068ed95444d484764b95953a04e69568c
maialele <mayalapi@gmail.com>
2022-10-11 21:51:16 +0300

    all modes work

--- etch.js
+++ etch.js
@@ -26,6 +26,7 @@ function makeSketchpad(rows, cols) {
     document.onmousedown = function() {
       isDown = true;
     }
+
     document.onmouseup = function() {
       isDown = false;
     }
@@ -50,18 +51,43 @@ function makeSketchpad(rows, cols) {
         }
       }
       //a simple click would also work.
-      cell.onclick = function() {
+      cell.onclick = () => {
+        cell.style.background = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
+      }
+    });
+
+    //brush options.
+    //randomize a color to draw with
+    randomButton.addEventListener('click', function() {
+      colorPicker.value = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
+      cell.onmouseover = function() {
+        if(isDown) {
+          cell.style.backgroundColor = colorPicker.value;
+        }
+      }
+      //a simple click would also work.
+      cell.onclick = () => {
         cell.style.background = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
       }
     });
     
+    //changes grid-item to white
+    eraserButton.addEventListener('click', function() {
+      cell.onmouseover = function() {
+        colorPicker.value = '#ffffff'
+        if(isDown) {
+          cell.style.backgroundColor = colorPicker.value;
+        }
+      }
+      //a simple click would also work.
+      cell.onclick = () => {
+        cell.style.background = '#ffffff'
+      }
+    });
+
     //clears the sketchpad.
     clearButton.addEventListener('click', clearMode);
 
-    //brush options.
-    randomButton.addEventListener('click', randomColor);
-    eraserButton.addEventListener('click', eraserMode);
-    
     container.appendChild(cell).className = "grid-item";
   };
 }
@@ -73,23 +99,13 @@ function reset(rows, cols) {
   colorPicker.value = "#000000"
 }
 
-
-//changes grid-items back into white.
-function eraserMode() {
-  colorPicker.value = '#ffffff';
-}
-
 //clears the sketchpad. changes all the grid-items to white. 
-function clearMode() {
+clearMode = () => {
   for (var i = 0; i < allGridItems.length; i++) {
     allGridItems[i].style.backgroundColor = '#ffffff';
   }
 }
 
-//chnages the color-picker to a random color.
-function randomColor() {
-  colorPicker.value = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
-}
 
 //default value of grid 
 makeSketchpad(12, 12)
